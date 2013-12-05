43dad5667f577ff102324dc588dbe13339b172f9
Yasmine.Gaber <yasmine.gaber@espace.com.eg>
2013-12-05 23:15:39 +0200

    initial creation

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mapleleaf2::Application.config.secret_token = '8c4d77afc281adbe213e6f10ea84d86171103d707b0f0eda40e315d80655ddaad3815b0cd3336052d72cd1a49ad9b9afbcc61558e640d368d9cc0c4d89aefdc5'
